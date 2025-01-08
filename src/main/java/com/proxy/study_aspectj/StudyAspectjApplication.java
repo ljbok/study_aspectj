f6cfc8aa6d8c7e677d2dfa4a7e37910b916162db
DESKTOP-UQNJGO7\LJB <dlwnsqhr123@gmail.com>
2025-01-08 18:35:28 +0900

    [ADD] init commit

---
+++ src/main/java/com/proxy/study_aspectj/StudyAspectjApplication.java
@@ -0,0 +1,13 @@
+package com.proxy.study_aspectj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAspectjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyAspectjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proxy/study_aspectj/StudyAspectjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxy.study_aspectj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAspectjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
